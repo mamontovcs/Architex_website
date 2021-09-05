8dab3c6ea4445ac54c0b78e00fe310f1000e1a63
mamontovcs <vintixfish@gmail.com>
2021-09-05 21:34:40 +0300

    Started working

---
+++ app.js
@@ -0,0 +1,65 @@
+$(function () {
+
+    /*Fixed header*/
+    let header = $("#header");
+    let intro = $("#intro");
+    let introHeight = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    checkScroll(introHeight, scrollPos);
+
+    $(window).on("scroll  resize", function () {
+
+        introHeight = intro.innerHeight();
+
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(introHeight, scrollPos);
+
+    });
+
+    function checkScroll(introHeight, scrollPos) {
+
+        if (scrollPos > introHeight) {
+            header.addClass("fixed");
+
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+
+    });
+
+    navToggle.on("click", function (event) {
+
+        event.preventDefault();
+        nav.toggleClass("show");
+
+    });
+
+    let slider = $("#reviewsSlider");
+
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+});
