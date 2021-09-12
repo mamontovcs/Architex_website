e5e38d5b889f92fe6a46c051f454040ecf88e7dd
mamontovcs <vintixfish@gmail.com>
2021-09-12 22:43:33 +0300

    added slider, which will be useful for me in future (mb)

--- app.js
+++ app.js
@@ -63,3 +63,13 @@ $(function () {
         dots: true
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // инициализация слайдера
+    new SimpleAdaptiveSlider('.slider', {
+        loop: true,
+        autoplay: false,
+        interval: 5000,
+        swipe: true,
+    });
+});
